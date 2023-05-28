83d54ffee5a3efb5f09307f841b975b230cdf558
markopdd <markpiddubnyi@gmail.com>
2023-05-28 11:48:38 +0300

    Added cities endpoint

---
+++ endpoints/cities.py
@@ -0,0 +1,45 @@
+from typing import Optional, List
+from fastapi import APIRouter
+from pydantic import BaseModel
+
+
+router = APIRouter()
+
+cities = [
+    {
+        "city_name" : "Kyiv",
+        "population" : "4 millions",
+        "is_capital" : true
+        "big_cities" : [
+            "Kyiv", "Kharkiv", "Odesa", "Dnipro", "Donetsk", "Lviv", "Sevastopol"],
+        "national_holidays" : 16,
+        "interesting_facts" : "The most beautiful country in the world!",            
+    }
+]
+
+class Country(BaseModel):
+    country_name: str
+    capital_name: str
+    population: str
+    language: str
+    big_cities: Optional[list]
+    national_holidays: Optional[int]
+    interesting_facts: Optional[str]
+
+
+@router.get("/countries", response_model=List[Country])
+async def get_all_countries():
+    return countries
+
+@router.post("/countries/")
+async def add_country(country: Country):
+    return countries.append(country)
+
+@router.get("/countries/country_names")
+async def get_all_country_names():
+    return [country.get("country_name") for country in countries]
+
+@router.get("/countries/{country_id}")
+async def get_country_id(country_id: int):
+    return countries[country_id]
+
--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-from typing import Optional, List
 from fastapi import FastAPI
-from pydantic import BaseModel
-
+from endpoints.countries import router
 
 app = FastAPI(
     title= "Capitals API",
@@ -11,42 +9,4 @@ app = FastAPI(
     license_info={"name":"MIT",},
 )
 
-class Country(BaseModel):
-    country_name: str
-    capital_name: str
-    population: str
-    language: str
-    big_cities: Optional[list]
-    national_holidays: Optional[int]
-    interesting_facts: Optional[str]
-
-
-countries = [
-    {
-        "country_name" : "Ukraine",
-        "capital_name" : "Kyiv",
-        "population" : "40 millions",
-        "language" : "Ukrainian",
-        "big_cities" : [
-            "Kyiv", "Kharkiv", "Odesa", "Dnipro", "Donetsk", "Lviv", "Sevastopol"],
-        "national_holidays" : 16,
-        "interesting_facts" : "The most beautiful country in the world!",            
-    }
-]
-
-@app.get("/countries", response_model=List[Country])
-async def get_all_countries():
-    return countries
-
-@app.post("/countries/")
-async def add_country(country: Country):
-    return countries.append(country)
-
-@app.get("/countries/country_names")
-async def get_all_country_names():
-    return [country.get("country_name") for country in countries]
-
-@app.get("/countries/{country_id}")
-async def get_country_id(country_id: int):
-    return countries[country_id]
-
+app.include_router(router)
